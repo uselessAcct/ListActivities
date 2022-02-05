7ce5766b06ab6ea32d475a4dc29f568e92695802
uselessAcct <chloegwonnn@gmail.com>
2022-02-04 18:00:04 -0800

    NEW COMMIT

--- app/src/main/java/com/example/myapplication/ArtActivity.java
+++ app/src/main/java/com/example/myapplication/ArtActivity.java
@@ -16,5 +16,7 @@ public class ArtActivity extends AppCompatActivity {
         TextView txtViewHrs = findViewById(R.id.txtViewHrs);
         txtViewHrs.setText("Info: http://www.naver.com");
         Linkify.addLinks(txtViewHrs, Linkify.WEB_URLS);
+        //LINKIFY
+        //COMMIT IS UPDATED
     }
 }
