391d1b022e521d45ccbc55ce59489ca3128420cc
uselessAcct <99013326+uselessAcct@users.noreply.github.com>
2022-02-04 18:01:08 -0800

    Update ArtActivity.java

--- app/src/main/java/com/example/myapplication/ArtActivity.java
+++ app/src/main/java/com/example/myapplication/ArtActivity.java
@@ -16,7 +16,6 @@ public class ArtActivity extends AppCompatActivity {
         TextView txtViewHrs = findViewById(R.id.txtViewHrs);
         txtViewHrs.setText("Info: http://www.naver.com");
         Linkify.addLinks(txtViewHrs, Linkify.WEB_URLS);
-        //LINKIFY
-        //COMMIT IS UPDATED
+        //THIS IS NEW COMMENT
     }
 }
